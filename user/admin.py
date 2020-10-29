16ce035420af99adf0030d44eef994e6fbd7c91c
Lukasz Chalinski <l.chalinski@gmail.com>
2020-10-29 18:43:44 +0100

    F003: last touch of User Registration

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'account_type', 'date_joined')
+    prepopulated_fields = {'slug': ('username',)}
+
+
+admin.site.register(Profile, ProfileAdmin)
+
--- user/models.py
+++ user/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.template.defaultfilters import slugify
+from user.choices import *
 
-# Create your models here.
+
+class Profile(AbstractUser):
+    slug = models.SlugField(null=False, unique=True)
+    photo = models.ImageField(blank=True, null=True, upload_to='profile_photos')
+    account_type = models.IntegerField(choices=ACCOUNT_TYPE, default=1)
+    about = models.TextField(blank=True, null=True, default='No info')
+    website_url = models.URLField(max_length=250, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.username)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name_plural = 'Profiles'
